dd8209cdc579943c98ab17372308fa8b1cc0fa80
zein3 <zeinhaddad02@gmail.com>
2022-01-09 07:30:30 +0700

    added chat bubble

---
+++ src/components/MessageBubble.js
@@ -0,0 +1,24 @@
+const MessageBubble = ({isFromMe, message, senderUsername, senderIp}) => {
+  const classes = ['p-2', 'max-w-lg', 'font-sans', 'text-xs', 'text-gray-200', 'rounded-sm', 'flex', 'flex-col'];
+  const conditionalClass = isFromMe ? 
+    ['bg-gray-900', 'self-end'] :
+    ['bg-green-900', 'self-start'];
+
+  const headerClasses = ['font-semibold', 'font-mono', 'text-blue-500', 'mb-1'];
+  const conditionalHeaderClass = isFromMe ? ['text-right'] : [];
+
+
+  return (
+    <div className={classes.concat(conditionalClass).join(' ')}>
+      <div className={headerClasses.concat(conditionalHeaderClass).join(' ')}>
+        <span>{senderUsername}</span>
+        <span>({senderIp})</span>
+      </div>
+      <div>
+        <p className="inline break-words">{message}</p>
+      </div>
+    </div>
+  )
+}
+
+export default MessageBubble
--- src/components/MessageWindow.js
+++ src/components/MessageWindow.js
@@ -1,16 +1,24 @@
+import MessageBubble from './MessageBubble'
+
 const MessageWindow = () => {
+  const aaaaa = "welkjflwaejflkwjeflaj;lkfjlsa;dkfjlaskdfj;lawkjelfkjasdl;kfjasl;kdfjalk;efj;lkasjdfl;kjsdlfk;jasd;lfkjas;ldkfjasl;kdfj;laskdjflaskdjf;laskdjfl;sakdjfl;kasdjflaksjdflaskjdflaskdfjaskldjf;aslkjdf;askljdf;laskjdf;laskdfj;laskdfj;laskjdf;laskjdf;laskdfj;lksdjf;laskdjfl;askjflsakdjflaskdjfl;asdkjfl;sakdjfl;askdjfl;askdjflasdkjflkj";
+
   return (
     <div class="pb-14 h-screen w-full">
-      <div class="p-2 h-full w-full bg-red-800 text-white overflow-y-auto">
+      <div class="p-2 h-full w-full bg-red-800 text-white overflow-y-auto flex flex-col">
 
-        {(() => {
-          const x = [];
-          for (let i = 0; i < 50; i++) {
-            x.push(<p key={i}>Lorem</p>)
-          }
-          return x;
-        })()}
-        <p>Last Lorem</p>
+        <MessageBubble
+          isFromMe={false}
+          message={aaaaa}
+          senderUsername="Username"
+          senderIp="192.168.1.1"
+        />
+        <MessageBubble
+          isFromMe={true}
+          message={aaaaa}
+          senderUsername="Username"
+          senderIp="192.168.1.1"
+        />
 
       </div>
     </div>
